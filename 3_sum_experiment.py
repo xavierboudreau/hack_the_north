85ed84619dcd4659e25b5901b55a4f2056f279fa
Dbajj <daniel.bajj@gmail.com>
2018-09-15 16:45:52 -0700

    Update 3-sum functionality

--- 3_sum_experiment.py
+++ 3_sum_experiment.py
@@ -2,80 +2,113 @@ from random import randint
 from random import sample
 
 def make_three_sum(clauses = 1000, variables = 32):
-	equation = []
-	for i in range(clauses):
-		X0 = randint(0, variables) * sample(set([1,-1]), 1)[0]
-		X1 = randint(0, variables) * sample(set([1,-1]), 1)[0]
-		while (X1 == X0):
-			X1 = randint(0, variables) * sample(set([1,-1]),1)[0]
-		X2 = randint(0, variables) * sample(set([1,-1]), 1)[0]
-		while (X2 == X1 or X2 == X0):
-			X2 = randint(0, variables) * sample(set([1,-1]), 1)[0]
-		equation.append([X0, X1, X2])
-	print(equation)
-	return equation
+    equation = []
+    for i in range(clauses):
+        X0 = randint(1, variables) * sample(set([1,-1]), 1)[0]
+        X1 = randint(1, variables) * sample(set([1,-1]), 1)[0]
+        while (X1 == X0):
+            X1 = randint(1, variables) * sample(set([1,-1]),1)[0]
+        X2 = randint(1, variables) * sample(set([1,-1]), 1)[0]
+        while (X2 == X1 or X2 == X0):
+            X2 = randint(1, variables) * sample(set([1,-1]), 1)[0]
+        equation.append([X0, X1, X2])
+    return equation
 
-def get_chunks(clauses, chunk_size):
-	input_size = 2**(3*clauses)
-	num_chunk_size_chunks = input_size//chunk_size
-	partial_chunk = input_size%chunk_size
-	return num_chunk_size_chunks, partial_chunk
+def solveable_three_sum(clauses = 1000, variables = 32):
+    equation = []
+    literal_map = {}
+    for i in range(clauses):
+        current_var = []
+        X0 = assign_variable(literal_map,current_var,variables)
+        current_var.append(X0)
+        X1 = assign_variable(literal_map,current_var,variables)
+        current_var.append(X1)
+        X2 = assign_variable(literal_map,current_var,variables)
+
+        equation.append([X0, X1, X2])
+
+    return equation
+
+def assign_variable(literal_map,current_var,variable_num):
+
+    variable = randint(1,variable_num)
+
+    if abs(variable) in literal_map:
+        variable = literal_map[abs(variable)]
+    else:
+        variable = variable * sample(set([1,-1]),1)[0]
+        literal_map[abs(variable)] = variable
+
+    while variable in current_var:
+        variable = randint(1,variable_num)
+
+        if abs(variable) in literal_map:
+            variable = literal_map[abs(variable)]
+        else:
+            variable = variable * sample(set([1,-1]),1)[0]
+            literal_map[abs(variable)] = variable
+
+    return variable
+
+
+
+def get_chunks(chunk_size,total_size):
+    chunks = []
+    cur = 0
+    while (cur <= total_size):
+        chunks.append((cur,cur+chunk_size))
+        cur += chunk_size
+    return chunks 
+
 
 def master():
-	clauses = 10
-	chunk_size = 10000000
-	equation = make_three_sum(clauses)
-	print(equation)
-	num_chunks, partial_chunk_size = get_chunks(clauses, chunk_size)
-	sum_ = partial_chunk_size + num_chunks*chunk_size
+    clauses = 10
+    variables = 24
+    chunks = get_chunks(2**10,2**24)
+#    equation = make_three_sum(clauses)
+    equation = solveable_three_sum(clauses,variables)
+    for chunk in chunks:
+        if client(equation,chunk[0],chunk[1],variables):
+            print("YAY")
+            return
 
-	result = False
+    print("BOO")
+    return
 
-	if (client(equation, 0, partial_chunk_size-1)):
-		result = True
-		return result
-	curr = partial_chunk_size
-	for i in range(num_chunks):
-		if client(equation, curr, curr+chunk_size-1):
-			result = True
-			return result
 
-	return result
+def client(equation, start, stop,num_variables):
+    #tests three sum from (start,stop]
 
-def try_permutation(equation, bin_array):
-	i = 0
-	for clause in equation:
-		T1 = bin_array[i] == clause[0]
-		T2 = bin_array[i+1] == clause[1]
-		T3 = bin_array[i+2] == clause[1]
-		result = T1 or T2 or T3
-		if not result:
-			return False
-		i += 3
-	print(bin_array)
-	return True
+    curr = start
+    while curr < stop:
+        if try_permutation(equation, curr,num_variables):
+            return True
+        curr += 1
 
-def to_binary(dec, num_terms):
-	#won't work I need the clause length
-	base = [int(x) for x in bin(dec)[2:]]
-	zero_pad = [0 for i in range(num_terms-len(base))]
-	result = zero_pad + base
-	return result
+    return False
 
-def client(equation, start, stop):
-	#tests three sum from start to stop (inclusive)
-	success = False
+def try_permutation(equation,curr,num_variables):
+    variables = []
+    for i in range(num_variables):
+        literal = curr % 2
+        if literal == 0:
+            literal = -1
+        variables.append((i+1)*literal)
+        curr = curr // 2
 
-	curr = start
-	num_terms = len(equation)*3
-	while curr <= stop:
-		curr_binary = to_binary(curr, num_terms)
-		if try_permutation(equation, curr_binary):
-			success = True
-			return success
-		curr += 1
 
-	return success
+
+    for clause in equation:
+        l0 = clause[0]
+        l1 = clause[1]
+        l2 = clause[2]
+        ass0 = variables[abs(l0)-1]
+        ass1 = variables[abs(l1)-1]
+        ass2 = variables[abs(l2)-1]
+        if l0 != ass0 or l1 != ass1 or l2 != ass2:
+            return False
+
+    return True
 
 if __name__ == "__main__":
-	make_three_sum()
+    master()

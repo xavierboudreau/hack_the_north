3d0a3f9f44cf07de837ef5c60e35e4252b0745a0
Xavier Boudreau <xavi.boudreau@gmail.com>
2018-09-15 19:10:51 -0400

    add requests for chunk on client side

--- client_side.py
+++ client_side.py
@@ -1,21 +1,59 @@
 import urllib.parse
 import urllib.request
 import psutil
+import json
+import sys
 
 def get_id(server_url):
-	get_request = "{}/{}".format(server_url, 'id')
-	content = urllib.urlrequest.urlopen(get_request).read()
+	get_request = "{}/{}/{}".format(server_url, 'api','id')
+	content = urllib.request.urlopen(get_request).read()
+	id = content.decode("utf-8")
 	#extract id and status code
+	return id
 	
 def get_chunk(server_url):
+	'''
+	Returns int start, int stop, list<list<int>> equation
+	Each variable x1, x2, x3, etc will be represented by 1, 2, 3, etc
+	A negative int represents the complement
+	e.g. -1 represents (NOT x1)
+	'''
 	parameters = urllib.parse.urlencode({"id": id})
 	parameters = parameters.encode("utf-8")
-	get_request = "{}/{}?{}".format(server_url, 'data', parameters)
-	content = urllib.urlrequest.urlopen(get_request).read()
+	get_request = "{}/{}/{}?{}".format(server_url, 'api', 'data', parameters)
+	content = urllib.request.urlopen(get_request).read()
+	json_result = content.decode("utf-8")
 	
-	#extract chunk, we don't know what this looks like yet
-	#we want to return None if there was no chunk provided
-	return content
+	try:
+		response_table = json.loads(json_result)
+		error_message = "COULD NOT FIND \'{}\' IN CHUNK RESPONSE. FAILING NOW"
+		try:
+			start = response_table["start"]
+		
+		except KeyError:
+			print(error_message.format("start"))
+			sys.exit()
+		
+		try:
+			stop = response_table["stop"]
+		except KeyError:
+			print(error_message.format("stop"))
+			sys.exit()
+		
+		try:
+			equation = response_table["equation"]
+			sys.exit()
+		except KeyError:
+			print(error_message.format("equations"))
+			sys.exit()
+		
+		return start, stop, equation
+		
+		
+	except json.JSONDecodeError:
+		print("COULDN'T DESERIALIZE CHUNK. FAILING NOW")
+		sys.exit()
+	
 
 def send_result(server_url, result, solution):
 	server_url = "{}/{}".format(server_url, "data")
@@ -31,7 +69,8 @@ def solve_chunk():
 	pass
 
 def main():
-	id = get_id()
+	server_url = "https://sadx-miner.herokuapp.com"
+	id = get_id(server_url)
 	chunk = get_chunk()
 	#assume we get None when there are no more chunks to process
 	while chunk != None:
@@ -40,7 +79,7 @@ def main():
 		while not received:
 			code = send_result()
 			#update received based on code
-			received = ?
+			received = -99
 	
 	#for now just terminate when we don't have a chunk to process
 	#later we can wait and check for a new one
@@ -48,4 +87,10 @@ def main():
 	
 
 if __name__ == "__main__":
-	main()
+	#main()
+	server_url = "https://sadx-miner.herokuapp.com"
+	get_id(server_url)
+	
+	
+	
+	

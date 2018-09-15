a864dd982ad2126ccb464f669453867b97bb79a5
Aram Baghdassarian <abagh0703@gmail.com>
2018-09-15 19:55:15 -0400

    added get and post api data for hte client

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import random
 app = Flask(__name__)
 
 random.seed()
-# userInfo is a dict with keys of their id and value
-userInfo = {}
+# chunkOfUser is a dict with keys of user's id and the chunk they're currently working on
+chunkOfUser = {}
 # computeStack is a "stack" with dicts of start and stop values to be sent to users
-computeStack = []
+computeStack = [{'start': 4000000, 'stop': 5000000, 'equation': [[]]}]
 # dummy values
 for x in range(0, 1000000 * 20, 1000000):
     equation = []
@@ -22,40 +22,59 @@ for x in range(0, 1000000 * 20, 1000000):
                 multiplier = 1
             equation[i].append(random.randint(1, 10) * multiplier)
     computeStack.append({'start': x, 'stop': x + 1000000, 'equation': equation})
-computeStack = [{'start': 4000000, 'stop': 5000000, 'equation': [[]]}]
 # clientTimeline is a priority queue to show the soonest client tasks first, priority is unix timestamp and value is
 # their userId
 clientTimeline = PriorityQueue()
 # seconds allowed for the user to do the computation. Now it's a constant, but one day it may be dynamic
 COMP_TIME_LIMIT = 15
+solution = {'solution': None}
 
 
 @app.route('/api/data', methods=['GET', 'POST'])
 def api_data():
     if request.method == 'GET':
         userId = request.args.get('id')
-        if userId is None:
-            return jsonify([]), 401
-        if userInfo[userId] is None:
-            return jsonify([]), 200
+        # if they didn't specify a user id, they're unauthorized
+        if userId is None or userId is '':
+            print(1)
+            return jsonify({}), 401
+        # if their userId was never put into the system, they're also unauthorized
+        if userId not in chunkOfUser:
+            print(chunkOfUser)
+            print(2)
+            return jsonify({}), 401
+        # they're in our system and already working on a compute part, so don't assign them a new one
+        if chunkOfUser[userId] is not None:
+            print(3)
+            return jsonify({'start': None, 'stop': None, 'equation': None}), 200
         # get their compute load and document it
         computeDatum = computeStack.pop()
-        userInfo[userId] = computeDatum
+        chunkOfUser[userId] = computeDatum
         # add their load to the priority queue
         clientTimeline.put(time.time() + COMP_TIME_LIMIT, userId)
         # send the value
         return jsonify(computeDatum)
     elif request.method == 'POST':
         data = request.form
-        pass  # Handle POST request
+        print('data')
+        print(data)
+        userId = request.args.get('id')
+        # if they didn't specify a user id, they're unauthorized
+        if userId is None or userId is '' or 'result' not in data:
+            return jsonify({}), 401
+        chunkOfUser[userId] = None
+        if data.result and 'solution' in data:
+            solution = data.solution
+#             TODO determine a procedure to do when solution is found...
 
 
 @app.route('/api/id', methods=['GET'])
 def api_id():
     if request.method == 'GET':
-        userId = uuid.uuid4()
-        userInfo[userId] = None
-        return str(userId)
+        userId = str(uuid.uuid4())
+        chunkOfUser[userId] = None
+        print(chunkOfUser)
+        return userId
 
 
 if __name__ == '__main__':

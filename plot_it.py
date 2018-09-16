2be0c9d81eb275c04448974df4c0889b34d179ea
Xavier Boudreau <xavi.boudreau@gmail.com>
2018-09-16 04:50:29 -0400

    add visualization

---
+++ plot_it.py
@@ -0,0 +1,43 @@
+import matplotlib as mpl
+mpl.use("TkAgg")
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from time import sleep
+
+def animate():
+	times = [0,1,2,3,4,5,6,7,8,9]
+	players = [[0,1,2,3,4,5,6,7,8,9], [0,1,2,3,4,5,6,7,8,9], [0,1,2,3,4,5,6,7,8,9], [4,1,6,3,4,7,1,7,9,9]]
+	player_ids = ["Player 1", "Player 2", "Player 3", "Player 4"]
+	bottoms = [[0 for i in range(len(players[0]))]]
+	
+	N = len(times)
+	ind = np.arange(N)
+	width = 0.35
+	bars = []
+	
+	for i in range(len(players)):
+		if i == 0:
+			bars.append(plt.bar(ind, players[i], width))
+			continue
+		else:
+			#bottom is where this bar should start. We keep track of the sum to stack the bars correctly
+			bottoms.append([players[i-1][t]+bottoms[i-1][t] for t in range(len(players[i]))])
+			bars.append(plt.bar(ind, players[i], width, bottom = bottoms[i]))
+	
+	
+	plt.title("Work Load Distribution by Player")
+	plt.yticks(np.arange(0, 101, 10))
+	plt.ylabel("Percent Completed (%)")
+	plt.xticks(ind, times)
+	plt.xlabel("Time Since Start (seconds)")
+	
+	plt.legend(tuple(bars), tuple(player_ids))
+	plt.show()
+
+if __name__ == "__main__":
+	#fig = plt.figure()
+	#ax1 = fig.add_subplot(1,1,1)
+	#ani = animation.FuncAnimation(fig, animate, interval = 1000)
+	animate()
+	

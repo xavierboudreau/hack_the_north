b4494d7b36ecdcedcba92581bb7474cfdb2d554f
Dbajj <daniel.bajj@gmail.com>
2018-09-16 00:22:57 -0700

    Stories will be told

--- app.py
+++ app.py
@@ -201,9 +201,9 @@ def api_data():
         lock.acquire()
         userId = request.args.get('id')
         # if they didn't specify a user id, they're unauthorized
-        if userId is None or userId is '' or userId not in chunkOfUser:
+        if userId is None or userId not in chunkOfUser:
             print(1)
-            return "invalid user id" + userId, 401
+            return "invalid user id " + userId + " " + chunkOfUser, 401
         # they're in our system and already working on a compute part, so don't assign them a new one
         if chunkOfUser[userId] is not None:
             print(3)
